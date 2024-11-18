301fbc8c29faff7d496d6c28711f29075470fe12
nic00la <nic00la.kaleta@gmail.com>
2024-11-18 12:54:30 +0100

    changed the size of window

--- Messenger-TEB-MAUI/App.xaml.cs
+++ Messenger-TEB-MAUI/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Messenger_TEB_MAUI
+﻿using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace Messenger_TEB_MAUI
 {
     public partial class App : Application
     {
@@ -7,6 +10,19 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+
+              protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 1400;
+            const int newHeight = 945;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
         }
     }
 }
